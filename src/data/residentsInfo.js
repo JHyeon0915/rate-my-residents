3d1e04c0db9925ca00794e6e4a38b6705b3567f0
Jeonghyeon Lee <jeonghyeon@Jeonghyeons-MacBook-Pro.local>
2022-12-07 20:51:10 -0800

    Add more info on resident's data

--- src/data/residentsInfo.js
+++ src/data/residentsInfo.js
@@ -1,15 +1,21 @@
 const residentsInfo = [
     {
         name: "Jeonghyeon Lee",
-        overallRate: 5
+        overallRate: 5,
+        quality_rate: 4,
+        difficulty_rate: 4
     },
     {
         name: "Yuki Krammer",
-        overallRate: 4
+        overallRate: 4,
+        quality_rate: 5,
+        difficulty_rate: 4
     },
     {
         name: "Julia Wise",
-        overallRate: 4.1
+        overallRate: 4.1,
+        quality_rate: 4,
+        difficulty_rate: 5
     }
 ]
 
