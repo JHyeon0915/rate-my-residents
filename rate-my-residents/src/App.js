6e7eef4593b9a17ba8771d6529053e197c00f661
Jeonghyeon <dlqjdgus99@naver.com>
2023-01-02 03:08:32 -0800

    Feature: connected server and client

--- rate-my-residents/src/App.js
+++ rate-my-residents/src/App.js
@@ -1,4 +1,5 @@
 import './App.css';
+import React from 'react';
 import Main from './pages/Main';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Rating from './pages/Rating';
@@ -6,16 +7,28 @@ import Result from './pages/Result';
 
 
 function App() {
+  const [data, setData] = React.useState(null);
+
+  React.useEffect(() => {
+    fetch("/")
+      .then((res) => res.json())
+      .then((data) => setData(data.message));
+  }, []);
+
   return (
-    <BrowserRouter>
-      <Routes>
-          <Route path="/" element={<Main />} >
-          <Route path="rating" element={<Rating />} />
-          <Route path="result" element={<Result />} />
-          </Route>
-      </Routes>
-    </BrowserRouter>
-    
+    <>
+      <BrowserRouter>
+        <Routes>
+            <Route path="/" element={<Main />} />
+            <Route path="/result" element={<Result />} />
+            <Route path="/result/rating" element={<Rating />} />
+        </Routes>
+      </BrowserRouter>
+      <form action="../../post" method="post" 
+              className="form">
+          <button type="submit">Connected?</button>
+      </form>
+    </>
   );
 }
 
--- server/index.js
+++ server/index.js
@@ -1,9 +1,39 @@
+//const path = require('path');
 const express = require("express");
+const db = require('./config/db');
 
 const PORT = process.env.PORT || 3001;
 
 const app = express();
 
+
+// Have Node serve the files for our built React app
+//app.use(express.static(path.resolve(__dirname, '../client/build')));
+
+app.get("/", (req, res) => {
+  res.json({ message: "Hello from server!" });
+});
+
+app.post("/post", (req,res)=>{
+  console.log("Connected to React");
+  res.redirect("/");
+});
+
+// All other GET requests not handled before will return our React app
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../client/build', 'index.html'));
+});
+
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
+
+// mysql
+app.get("/api/get", (req,res)=>{
+  db.query("SELECT * FROM rate_my_residents", (err,result)=>{
+    if(err)
+      console.log(err)
+  res.send(result)
+  });
+});
+
