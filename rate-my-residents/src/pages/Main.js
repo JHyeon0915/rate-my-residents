5d8cbc58f58ef9bd4ea36248d7dc867df787704c
Jeonghyeon <dlqjdgus99@naver.com>
2023-01-06 05:30:34 -0800

    Modify main page with styled component

--- rate-my-residents/src/pages/Main.js
+++ rate-my-residents/src/pages/Main.js
@@ -1,14 +1,28 @@
 import React from 'react';
 import {Outlet, Link} from 'react-router-dom';
 import Search from '../components/Search';
+import styled from 'styled-components';
+import main_pic from '../assets/Images/main_picture.jpeg';
+
+const Wrapper = styled.div`
+    width: 100%;
+    height: 700px;
+    padding-top: 5.0rem;
+`;
+
+const Image = styled.img`
+    position: relative;
+    width: 100%;
+    height: 0.5 rem
+`;
 
 function Main(){
     return(
-        <>
-        <div>RATE MY RESIDENT</div>
-        <Search></Search>
-        <Outlet />
-        </>
+        <Wrapper>
+            <Image src={main_pic}/>
+            <Search></Search>
+            <Outlet />
+        </Wrapper>
     );
 }
 
