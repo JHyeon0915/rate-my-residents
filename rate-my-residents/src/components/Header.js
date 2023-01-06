d3457084030ac4cf20ea84b6684729ad1da85f2e
Jeonghyeon <dlqjdgus99@naver.com>
2023-01-06 05:30:09 -0800

    Modify header with styled component

--- rate-my-residents/src/components/Header.js
+++ rate-my-residents/src/components/Header.js
@@ -1,7 +1,19 @@
+import styled from "styled-components";
+
+const Wrapper = styled.section`
+    margin: 0;
+    padding: 0;
+    top: 0;
+    height: 64px;
+    width: 100%;
+    background: black;
+    color: white;
+    `;
+
 export default function Header(){
     return(
-        <>
-            <div>here is header</div>
-        </>
+        <Wrapper>
+                <h3 style={{textAlign: "center", padding: "20px"}}>R M R</h3>
+        </Wrapper>
     )
 }
