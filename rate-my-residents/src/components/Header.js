d1dfcdf0e5f0695e6b34364528cb35024bd42296
Jeonghyeon <dlqjdgus99@naver.com>
2023-01-11 06:22:47 -0800

    Add RMR button

--- rate-my-residents/src/components/Header.js
+++ rate-my-residents/src/components/Header.js
@@ -1,19 +1,33 @@
 import styled from "styled-components";
+import { Link } from "react-router-dom";
 
+export default function Header(){
+    return(
+        <Wrapper>
+                <RMR style={{}}>
+                    <Link to="/" style={{textDecoration: "none", color: "black"}}>  
+                        <b>R M R</b>
+                    </Link>
+                </RMR>
+        </Wrapper>
+    )
+}
 const Wrapper = styled.section`
     margin: 0;
-    padding: 0;
+    padding-bottom: 0;
     top: 0;
     height: 64px;
     width: 100%;
     background: black;
     color: white;
-    `;
+    align-items: center;
+`;
 
-export default function Header(){
-    return(
-        <Wrapper>
-                <h3 style={{textAlign: "center", padding: "20px"}}>R M R</h3>
-        </Wrapper>
-    )
-}
+const RMR = styled.div`
+    textAlign: center;
+    padding: 0;
+    background: white;
+    font-size: 16px;
+    width: 70px;
+    height: 30px;
+`;
