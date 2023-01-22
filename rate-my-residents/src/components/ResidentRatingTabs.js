faf03fdb26bccc3d8d97d4f8c165fa979508d13a
Jeonghyeon <dlqjdgus99@naver.com>
2023-01-22 11:10:12 -0800

    Modify rating tabs

--- rate-my-residents/src/components/ResidentRatingTabs.js
+++ rate-my-residents/src/components/ResidentRatingTabs.js
@@ -5,34 +5,57 @@ import styled from 'styled-components';
 
 
 function ResidentRatingTabs(props) {
-    console.log(props.quality_rate);
+    props = props.props;
+    console.log(props);
+
+    const tagElements = [];
+
+    if(props.tag1)
+        tagElements.push(<Tag>{props.tag1}</Tag>);
+    if(props.tag2)
+        tagElements.push(<Tag>{props.tag2}</Tag>);
+    if(props.tag3)
+        tagElements.push(<Tag>{props.tag3}</Tag>);
+
     return(
     <Wrapper>
-        <div>
+        <CardNumContainer>
             <CardNumRating title="QUALITY" rate={props.quality_rate}></CardNumRating>
             <CardNumRating title="DIFFICULTY" rate={props.difficulty_rate}></CardNumRating>
-        </div>
-        <div>
+        </CardNumContainer>
+        <GeneralRateInfoContainer>
             <div><b>COMMENT</b></div>
-            {props.comment}
-            <Tag><b>{props.tag1}</b></Tag>
-            <Tag>{props.tag2}</Tag>
-            <Tag>{props.tag3}</Tag>
-        </div>
+            <p>{props.comment}</p>
+            {tagElements}
+        </GeneralRateInfoContainer>
     </Wrapper>
     )
 }
 
 const Wrapper = styled.div`
-    background: #F1F1F1;
+    max-width: 882px;
+    background: #F9F9F9;
+    margin: 5px;
+    padding: 0;
 `;
 
 const Tag = styled.div`
     display: inline-block;
-    background: #F1F1F1;
+    background-color: #F1F1F1;
     margin: 10px 10px 0 5px;
     padding: 10px 15px;
-    border-radius: 15px;
+    border-radius: 25px;
+    font-weight: bold;
+`;
+
+const CardNumContainer = styled.div`
+    display: inline-block;
+    margin: 25px;
+`;
+
+const GeneralRateInfoContainer = styled.div`
+    display: inline-block;
+    margin: 25px;
 `;
 
 
