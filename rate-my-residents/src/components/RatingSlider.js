a3ccef2c6da6b40247da7a468d67e425bbd71b7b
Jeonghyeon <dlqjdgus99@naver.com>
2023-02-07 12:07:36 -0800

    Change RatingSlider to look more similar to original site

--- rate-my-residents/src/components/RatingSlider.js
+++ rate-my-residents/src/components/RatingSlider.js
@@ -1,22 +1,79 @@
-import { useState } from 'react';
-import RatingMui from '@mui/material/Rating';
+import React, { useState } from "react";
+import styled from "styled-components";
 
 function RatingSlider(props){
-    const [value, setValue] = useState(0);
+  const [rating, setRating] = useState(0);
+  const [hover, setHover] = useState(0);
+  const rateColors = ["red", "orange", "yellow", "olive", "green"];
 
-    return(
-      <>
-        <RatingMui
-          name="simple-controlled"
-          value={value}
-          size="large"
-          onChange={(event, newValue) => {
-            setValue(newValue);
-            props.setRate(newValue)
-          }}
-        />
-      </>
-    );
+  return (
+      <Wrapper className="star-rating">
+        {[...Array(5)].map((block, index) => {
+          index += 1;
+          return (
+            <button
+              type="button"
+              key={index}
+              className={index <= (hover || rating) ? "on"+index : "off"+index}
+              onClick={() => {
+                setRating(index)
+              }}
+              onMouseEnter={() => setHover(index)}
+              onMouseLeave={() => setHover(rating)}
+            />
+          );
+        })}
+      </Wrapper>
+  );
+};
+
+const Wrapper = styled.div`
+button {
+  width: 60px;
+  height: 40px;
+  margin-top: 30px;
+  margin-right: 2px;
+  background-color: #F2F2F2;
+  border: none;
+  outline: none;
+  cursor: pointer;
 }
+.on1 {
+opacity: 0.5;
+background-color: red;
+}
+.off1 {
+color: none;
+}
+.on2 {
+opacity: 0.5;
+background-color: orange;
+}
+.off2 {
+  color: none;
+}
+.on3 {
+  opacity: 0.5;
+  background-color: yellow;
+}
+.off3 {
+
+}
+.on4 {
+  opacity: 0.5;
+  background-color: LimeGreen;
+}
+.off4 {
+
+}
+.on5 {
+  opacity: 0.5;
+  background-color: DarkGreen;
+}
+.off5 {
+
+}
+`;
+
 
 export default RatingSlider;
