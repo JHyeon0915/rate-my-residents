9b5c0b1861050a3e1a2a15131b2caa97d395239f
Jeonghyeon <dlqjdgus99@naver.com>
2023-02-16 16:54:54 -0800

    Modify css style of form card

--- rate-my-residents/src/components/FormCard.js
+++ rate-my-residents/src/components/FormCard.js
@@ -3,23 +3,26 @@ import RatingSlider from "./RatingSlider";
 import RatingRadioButton from "./RatingRadioButton";
 
 function FormCard (props) {
-    console.log(props);
-
     return(
         <Wrapper>
-            <div style={{fontSize: "20px", margin: "5px 0"}}><b>{props.title}</b></div>
+            <div>
+                <p><b>{props.title}</b></p>
+            </div>
             {props.type === "slider" ? <RatingSlider setRate={props.setRate} /> : <RatingRadioButton setRate={props.setRate} />}
         </Wrapper>
     );
 }
 
 const Wrapper = styled.div`
-    padding: 20px;
-    height: 154px;
+    padding: 30px 0px 0px;
+    max-width: 886px;
+    width: 100%;
+    height: 156px !important;
     border: 1px solid #D7D7D7;
     box-shadow: 0px 5px 10px #D7D7D7;
     border-radius: 5px;
     margin: 15px 0px;
+    font-size: 16px;
 `;
 
 export default FormCard;
