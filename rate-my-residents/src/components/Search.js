a5c2cdfbd404cff6061ca9aa26c8cadaa752d901
Jeonghyeon Lee <dlqjdgus99@naver.com>
2023-08-16 02:55:07 +0900

    style: organize css styles for main page

--- rate-my-residents/src/components/Search.js
+++ rate-my-residents/src/components/Search.js
@@ -3,17 +3,21 @@ import AutoSuggestion from "./AutoSuggestion.js";
 import styled from "styled-components";
 
 function Search(){
-    return(
-        <MainWrapper>
-            <h1 style={{"color": "white", "font-size": "30px"}}>Find a resident at SJSU International House</h1>
-            <AutoSuggestion />
-        </MainWrapper>
-    );
+  return(
+    <MainWrapper>
+      <h1 style={{"color": "white", "font-size": "30px"}}>Find a resident at SJSU International House</h1>
+      <AutoSuggestion />
+    </MainWrapper>
+  );
 }
 
 const MainWrapper = styled.div`
-    left: 0;
-    width: 100%;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    height: fit-content;
+    padding: 0;
 `;
 
 export default Search;
--- rate-my-residents/src/pages/Main.js
+++ rate-my-residents/src/pages/Main.js
@@ -5,34 +5,37 @@ import styled from 'styled-components';
 import main_pic from '../assets/Images/main_picture.jpeg';
 
 function Main(){
-    return(
-        <Wrapper>
-            <Image src={main_pic}/>
-            <SearchBox className="search-box">
-                <Search />
-            </SearchBox>
-        </Wrapper>
-    );
+  return(
+    <Wrapper>
+      <Background>
+        <SearchBox>
+          <Search />
+        </SearchBox>
+      </Background>
+    </Wrapper>
+  );
 }
 
 const Wrapper = styled.div`
-    width: 100%;
-    height: 700px;
-    padding-top: 5.0rem;
+  width: 100%;
+  height: 700px;
+  padding-top: 5.0rem;
 `;
 
-const Image = styled.img`
-    position: relative;
-    width: 100%;
-    height: 600px;
-    filter: brightness(50%);
+const Background = styled.div`
+  display: flex;
+  justify-content: center;
+  width: 100%;
+  height: 600px;
+  background-size: 100% 100%;
+  background-image: linear-gradient(rgba(0, 0, 0, 0.527), rgba(0, 0, 0, 0.5)), url(${main_pic});
+
 `;
 
 const SearchBox = styled.div`
-    position: absolute;
-    left: 0;
-    top: 40%;
-    width: auto;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 export default Main;
