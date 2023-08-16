e9af1e96490b4350e671b49d1bd6ec12944f2ed3
Jeonghyeon Lee <dlqjdgus99@naver.com>
2023-08-16 23:01:18 +0900

    style: RMR in header

--- rate-my-residents/src/components/Header.js
+++ rate-my-residents/src/components/Header.js
@@ -5,29 +5,31 @@ export default function Header(){
     return(
         <Wrapper>
                 <RMR style={{cursor: "pointer"}}>
-                    <Link to="/" style={{textDecoration: "none", color: "black"}}>  
-                        <b>R M R</b>
+                    <Link to="/" style={{ textDecoration: "none", color: "black" }}>  
+                        <p><b>R M R</b></p>
                     </Link>
                 </RMR>
         </Wrapper>
     )
 }
 const Wrapper = styled.section`
-    margin: 0;
-    padding: 10px 0;
-    top: 0;
-    height: 45px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
     width: 100%;
+    height: 45px;
+    margin: 0;
+    padding: 1rem 0;
     background: black;
     color: white;
-    align-items: center;
 `;
 
 const RMR = styled.div`
-    textAlign: center;
-    padding-top: 10px;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 70px;
+    height: 3rem;
     background: white;
     font-size: 16px;
-    width: 70px;
-    height: 30px;
 `;
