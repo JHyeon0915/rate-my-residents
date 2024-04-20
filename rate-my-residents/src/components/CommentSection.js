531cbae60ab3099a030dfaa395f14fecc7e81617
Jeonghyeon Lee <dlqjdgus99@naver.com>
2024-04-20 20:34:42 +0900

    design: guidance section in Rating page

--- rate-my-residents/src/components/CommentSection.js
+++ rate-my-residents/src/components/CommentSection.js
@@ -1,25 +1,45 @@
+import { useState } from 'react';
 import styled from "styled-components";
-import alert_pic from '../assets/Images/alert.svg';
 
 function CommentSection (props) {
     const PLACEHOLDER = "What do you want to share? Feel free to leave comments of glorious moments with your lovely resident.";
-    
+    const [isGuidanceOpen, setIsGuidanceOpen] = useState(false);
+
     const onChange = (e) => {
         props.setComment(e.target.value);
     }
 
+    const toggleGuidance = () => {
+        setIsGuidanceOpen(!isGuidanceOpen);
+    };
+
     return(
         <Wrapper>
-            <div style={{font: "16px"}}><b>Write a Review</b></div>
-            <p>Discuss the professor's professional abilities including teaching style and ability to convey the material clearly</p>
-            <Guidance>
-                <image src={alert_pic}/>
-                <b>Guidelines</b>
-                    <ol>
+            <Title>Write a Review</Title>
+            <Content>Discuss the professor's professional abilities including teaching style and ability to convey the material clearly</Content>
+            
+            <GuidanceContainer>
+                <GuidanceHeader onClick={toggleGuidance}>
+                    <div>
+                        <h2 style={{fontSize: '14px'}}>
+                            <span style={{fontSize: '18px'}}>⚠ </span>
+                            Guidelines
+                        </h2>
+                    </div>
+                    <div style={{cursor: 'default', userSelect: 'none'}}>{isGuidanceOpen ? '▲' : '▼'}</div>
+                </GuidanceHeader>
+                {isGuidanceOpen && <GuidanceContent>
+                    <ul>
                         <li>Please be thoughtful not to hurt anyone.</li>
-                    </ol>
-            </Guidance>
+                        <li>Do not use inappropriate language.</li>
+                        <li>Do not post personal information.</li>
+                    </ul>
+                </GuidanceContent>}
+            </GuidanceContainer>
+            <div>
             <CommentArea onChange={onChange} type="text" placeholder={PLACEHOLDER}></CommentArea>
+            </div>
+            
         </Wrapper>
     );
 }
@@ -32,18 +52,66 @@ const Wrapper = styled.div`
     padding: 20px;
 `;
 
-const Guidance = styled.div`
+const Title = styled.div`
+    font-size: 16px;
+    font-weight: 700;
+    line-height: 20px;
+    margin-bottom: 16px;
+    text-align: left;
+`;
+
+const Content = styled.div`
+    font-size: 16px;
+    line-height: 22px;
+    margin-bottom: 16px;
+    text-align: left;
+`;
+
+const GuidanceContainer = styled.div`
     background: #F7F7F7;
-    margin: 20px 0;
+    background: rgb(247, 247, 247);
+    border-radius: 3px;
+    display: flex;
+    flex-direction: column;
+    font-size: 16px;
+    line-height: 22px;
+    margin-bottom: 16px;
+`;
+
+const GuidanceHeader = styled.div`
+    align-items: center;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    padding: 12px 22px 12px 10px;
+    text-align: left;
+    font-weight: 700;
+`;
+
+const GuidanceContent = styled.div`
+    padding: 0px 22px 22px 60px;
+    text-align: left;
+
+  & > ul {
+    font-size: 16px;
+    list-style: disc;
+    margin-bottom: 8px;
+  }
+
+  & > ul > li {
+    text-align: left;
+  }
 `;
 
 const CommentArea = styled.textarea`
-    width: 94%;
+    width: 96%;
     height: 201px;
-    padding: 20px;
     text-align: left;
     word-break: break-word;
     resize: none;
+    font-size: 16px;
+    padding: 2%;
+    border-radius: 3px;
 `;
 
 export default CommentSection;
